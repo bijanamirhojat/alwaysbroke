d2b3d54f7f7e25fbc23f08d0d9a3fa532de446e6
Bijan Amir-Hojat <amirhojat@mediamarkt.nl>
2018-11-11 22:57:54 +0100

    start

---
+++ alwaysbroke/Controller/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  alwaysbroke
+//
+//  Created by Bijan Amir-Hojat on 11/11/2018.
+//  Copyright © 2018 bCreate. All rights reserved.
+//
+
+import Foundation
--- alwaysbroke/Controller/ViewController.swift
+++ alwaysbroke/Controller/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var navbar: UINavigationItem!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navbar.title = "test"
+        
     }
-
 
 }
 
